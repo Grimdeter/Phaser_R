53f24fad35ff6d5cbf8af0e78b1165a55a889789
Grimdeter <66075155+Grimdeter@users.noreply.github.com>
2020-08-09 14:51:44 +0600

    a

--- client/src/scenes/gamePhase1.js
+++ client/src/scenes/gamePhase1.js
@@ -66,7 +66,7 @@ export default class gamePhase1 extends Phaser.Scene
         this.dealer = new Dealer(this)
     
         //                http://localhost:3000 if local, 5000 for local heroku
-        this.socket = io('')
+        this.socket = io('https://ancient-caverns-28613.herokuapp.com')
 
         // tells the client which player is which 
         this.socket.on('connect', () =>
